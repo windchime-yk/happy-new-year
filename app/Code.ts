0419fc71e956d5458fe95dcb6424adcc7b9ade72
WhyK <windchimeyk@gmail.com>
2025-04-29 14:36:34 +0900

    refactor: update deps and refactor main code (#3)
    * chore: add volta config
    
    * chore: replace yarn to pnpm
    
    * chore: update deps
    
    * refactor: update main code
    
    * ci: update renovate config
    
    * chore: adopt Biome
    
    * style: biome check

--- app/Code.ts
+++ app/Code.ts
@@ -1,22 +1,29 @@
-type Env = 'WEBHOOK_URL'
+type Env = 'WEBHOOK_URL';
 
-const getEnvProperty = (property: Env): string => PropertiesService.getScriptProperties().getProperty(property)
+/** GASから環境変数を引き出す */
+const getEnvPropertyOrThrow = (property: Env): string => {
+  const env = PropertiesService.getScriptProperties().getProperty(property);
+  if (!env) throw new Error(`Environment variable ${property} is not set`);
+  return env;
+};
 
+/** Slack Webhook経由でSlackに投稿する */
 const postToSlack = (): void => {
   const payload = {
-    text: '<!channel> Happy New Year!! :tada:'
-  }
-  const url = getEnvProperty('WEBHOOK_URL')
+    text: '<!channel> Happy New Year!! :tada:',
+  };
+  const url = getEnvPropertyOrThrow('WEBHOOK_URL');
   const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
     method: 'post',
     contentType: 'application/json',
-    payload: JSON.stringify(payload)
-  }
+    payload: JSON.stringify(payload),
+  };
 
-  UrlFetchApp.fetch(url, options)
-}
+  UrlFetchApp.fetch(url, options);
+};
 
+/** 新年の挨拶をする */
 const postNewYear = (): void => {
-  const nowDate = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'MM/dd')
-  if (nowDate === '01/01') postToSlack()
-}
+  const nowDate = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'MM/dd');
+  if (nowDate === '01/01') postToSlack();
+};
